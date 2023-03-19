4e0ea6f0a65e47e330895dde036f200863379989
İsmail <idincer944@gmail.com>
2023-03-19 21:36:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// function expression called divide
+const divide = function(a, b) {
+    return a / b;
+};
+
+// arrow function for to get square of any number
+const square = x => x * x;
+
+// arrow function to add two numbers
+const add = (a, b) => a + b;
